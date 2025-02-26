15b914bdd29cf71887d30f9b161619a78dd4abdf
ShaffaDwiaji <amagiri7141@google.com>
2025-02-26 14:57:03 +0700

    Update intent ke halaman Login

--- app/src/main/java/com/currency/miniproject1/MainActivity.kt
+++ app/src/main/java/com/currency/miniproject1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.currency.miniproject1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val backButton: Button = findViewById(R.id.back)
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
